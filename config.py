3cb4233054a895ada8cd04b8365ae5a3cf543d72
Shazman Malik <=>
2019-09-30 00:08:53 +0530

    changing requirements.txt mysql

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
 
     SECRET_KEY = "This Is My Key"
-    SQLALCHEMY_DATABASE_URI = os.environ.get('CLEARDB_DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SERVER_NAME = 'imdb-shazman.herokuapp.com'
 
